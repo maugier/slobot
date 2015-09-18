e52b53a633b80e40bd997ec6b4f1b32d494ea9fb
Maxime Augier <max@xolus.net>
2015-09-18 21:16:20 +0200

    Wrote core skeleton, start implementing IRC side, start testing

--- slobot/slobot.py
+++ slobot/slobot.py
@@ -1,17 +1,17 @@
 #!/usr/bin/env python3
 
-from irc.bot import SingleServerIRCBot
+import irc.bot
 import argparse
+from logging import debug, info, warn, error
 import sys
-import traceback
 import threading
 import yaml
 
 
-def log(*k, **a):
-    print(*k, file=sys.stderr, **a)
-
-mandatory_config = {'irc': ['nick', 'server'], 'jabber': ['nick', 'id', 'password'], 'fifo': ['path']}
+mandatory_config = {'irc': ['nick', 'server'], 
+                    'jabber': ['nick', 'id', 'password'],
+                    'fifo': ['path'],
+                    'console': [] }
 
 class ConfigurationError(Exception):
     pass
@@ -44,27 +44,34 @@ class Config:
             raise ConfigurationError("Config is missing a routing section")
 
         for route in routes:
-            for (sock, chan) in route.items():
-                if sock not in sockets:
-                    raise ConfigurationError("route link includes an unknown socket name '{0}'".format(link))
+            for (key, chan) in route.items():
+                sock = sockets.get(key)
 
-                if sockets['sock']['type'] == 'irc':
+                if sock is None:
+                    raise ConfigurationError("route link includes an unknown socket name '{0}'".format(key))
+
+                if sock['type'] == 'irc':
                     if chan[0] != '#':
                         raise ConfigurationError("IRC Channels should start with #")
 
-                if sockets['sock']['type'] == 'xmpp':
+                elif sock['type'] == 'xmpp':
                     pass #TODO check correct format for xmpp channel
+
+        self.sockets = sockets
+        self.routes = routes
 
 class DummyRouter:
     """ A debugging router that will simply dump incoming messages to stderr """
     def receive(self, chan, message):
-        log("[{0}] {1}".format(chan, message))
+        print("[{0}] {1}".format(chan, message), file=sys.stderr)
 
 
 class Socket():
     def __init__(self, router, key, config, readonly=False):
         self.readonly = readonly
         self.key = key
+        self._config = config
+        self._channels = set()
         if router is None:
             self._router = DummyRouter()
         else:
@@ -77,9 +84,17 @@ class Socket():
         raise NotImplementedError
 
     def receive(self, chan, message):
+        debug
         self._router.receive(self, chan, message)
 
+    def register(self, chan):
+        debug("Socket {0} registering {1}", self.key, chan)
+        self._channels.append(chan)
+
     def send(self, chan, message):
+        raise NotImplementedError
+
+    def users(self):
         raise NotImplementedError
 
 
@@ -90,26 +105,42 @@ class Console(Socket):
     def start():
         return
     def receive(self, chan, message):
-        log("[{0}] {1}".format(chan, message))
+        info("[{0}] {1}".format(chan, message))
 
 
 class FIFO(Socket):
-    def __init__(self, router, key, config):
-        super().__init__(router, key, config, True)
-        self._path = config['path']
-
     def run(self):
         while(True):
             try:
-                with open(self._path, 'r') as handle:
+                with open(self._config['path'], 'r') as handle:
                     for line in handle:
                         self.receive("*", ('notice', line))
                 
             except Exception as e:
-                traceback.print_stack()
+                logging.exception("Could not read from fifo")
 
 class IRC(Socket):
-    pass
+    def run(self):
+        nick = self._config['nick']
+        server = self._config['server']
+        user = self._config.get('user', 'slobot')
+        real = self._config.get('real', 'SLoBot Chat Bridge')
+
+
+        class Bot(irc.bot.SingleServerIRCBot):
+            def on_welcome(bot, c, e):
+                for chan in self._channels:
+                    c.join(chan)
+
+            def on_pubmsg(bot, c, e):
+                self.receive(e.target, ('message', e.arguments[0]))
+
+            def on_notice(bot, c, e):
+                self.receive(e.target, ('notice', e.arguments[0]))
+
+        bot = Bot(irc.bot.ServerSpec(server, 6667), nick, real)
+        bot.start()
+    
 
 class XMPP(Socket):
     pass
@@ -127,6 +158,8 @@ class Router:
         self._sockets = {}
         for (key, conf) in sockets.items():
             self._sockets[key] = socket_types[conf['type']](self,key,conf)
+        for (key, chan) in routes.items():
+            self._sockets[key].register(chan)
 
 
     def receive(self, source, source_chan, message):
@@ -139,6 +172,10 @@ class Router:
                     if dest.readonly:
                         next
                     dest.send(dest_chan, message)
+
+    def start(self):
+        for (key, socket) in self._sockets:
+            socket.start()
 
 def main():
     parser = argparse.ArgumentParser(description="Swisslinux.org's Jabber/IRC Bridge")
